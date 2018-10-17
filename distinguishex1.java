1db49f5e7ca51892443e82276c29aa22499eb810
altorious <altorious153@gmail.com>
2018-10-17 11:57:37 +0900

    Add files via upload

---
+++ distinguishex1.java
@@ -0,0 +1,20 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class distinguishex1 {
+		public static void main(String[] args) throws IOException {
+		
+			FileInputStream in = null;
+		
+			// Initialize stream 
+		
+			byte data;
+		
+			while ((data = (byte) in.read()) != -1) { 
+		
+			
+				// ... 
+				}
+			}
+		
+}
